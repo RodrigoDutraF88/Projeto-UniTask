4b3944fbf11f1b08e0a18a9d149152a28cf9e531
RodrigoDutraF88 <rodrigodutraf88@gmail.com>
2026-02-09 17:18:29 -0300

    modificando frontend e Login.js

--- mobile/App.js
+++ mobile/App.js
@@ -1,6 +1,6 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
-import Login from './src/screens/Login';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, View } from "react-native";
+import Login from "./src/screens/login";
 
 export default function App() {
   return (
@@ -14,8 +14,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#383737',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
+
+
--- mobile/src/api/api.js
+++ mobile/src/api/api.js
@@ -1,7 +1,22 @@
-import axios from 'axios';
+import axios from "axios";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
+// Cria a instância do axios apontando para o backend
 const api = axios.create({
-    baseURL: 'http://SEU_IP:3000',
+  baseURL: "http://SEU_IP:3000", // IP da sua máquina
+});
+
+// Antes de toda requisição
+api.interceptors.request.use(async (config) => {
+  // Busca o token salvo
+  const token = await AsyncStorage.getItem("@token");
+
+  // Se existir token, envia no header Authorization
+  if (token) {
+    config.headers.Authorization = `Bearer ${token}`;
+  }
+
+  return config;
 });
 
 export default api;
